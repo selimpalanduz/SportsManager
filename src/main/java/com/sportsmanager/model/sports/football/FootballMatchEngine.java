fd37af913b5276ba87072f72a3f169c88b8d7ef6
Mustafa <mustafasen0406@hotmail.com>
2026-04-09 22:55:01 +0300

    I did FootballSport class .I created override methods.

--- src/main/java/com/sportsmanager/model/sports/football/FootballMatchEngine.java
+++ src/main/java/com/sportsmanager/model/sports/football/FootballMatchEngine.java
@@ -1,4 +1,65 @@
 package com.sportsmanager.model.sports.football;
 
-public class FootballMatchEngine {
+import com.sportsmanager.interfaces.IMatchEngine;
+import com.sportsmanager.model.common.Match;
+import com.sportsmanager.model.common.MatchResult;
+import com.sportsmanager.model.common.PeriodResult;
+import com.sportsmanager.model.common.Player;
+
+public class FootballMatchEngine implements IMatchEngine {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/sportsmanager/model/sports/football/FootballSport.java
+++ src/main/java/com/sportsmanager/model/sports/football/FootballSport.java
@@ -1,4 +1,26 @@
 package com.sportsmanager.model.sports.football;
 
-public class FootballSport {
+import com.sportsmanager.interfaces.ISport;
+
+public class FootballSport implements ISport {
+    @Override
+    public String getSportName() { return "Football"; }
+
+    @Override
+    public int getNumberOfPeriods() { return 2; }
+
+    @Override
+    public int getPeriodDurationMinutes() { return 45; }
+
+    @Override
+    public int getPointsForWin() { return 3; }
+
+    @Override
+    public int getPointsForDraw() { return 1; }
+
+    @Override
+    public int getAllowedSubstitutions() { return 5; }
+
+    @Override
+    public int getSquadSize() { return 11; }
 }
