03cbc24d77f7ab2dd8bfd52263a29a7afdfccb1a
Selim Palanduz <selimpalanduz13@gmail.com>
2026-04-10 18:29:11 +0300

    SportFactory added and maven compiles too.

--- src/main/java/com/sportsmanager/core/SportFactory.java
+++ src/main/java/com/sportsmanager/core/SportFactory.java
@@ -1,5 +1,21 @@
 package com.sportsmanager.core;
 
+import com.sportsmanager.interfaces.IMatchEngine;
+import com.sportsmanager.interfaces.ISport;
+import com.sportsmanager.model.sports.football.FootballMatchEngine;
+import com.sportsmanager.model.sports.football.FootballSport;
 public class SportFactory {
+    public static ISport createSport(String sportType){
+        if(sportType.equalsIgnoreCase("football")){
+            return new FootballSport();
+        }
+        throw new IllegalArgumentException("Unknown sport: " +sportType);
+    }
+    public static IMatchEngine createEngine(String sportType){
+        if(sportType.equalsIgnoreCase("football")){
+            return new FootballMatchEngine();
+        }
+        throw new IllegalArgumentException("Unknown sport:" + sportType);
+    }
     //Selim
 }
