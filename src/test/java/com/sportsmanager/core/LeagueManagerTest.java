dded6b8771e6e178e265feaa3cfe83a6a5ba0e1e
DenizKaraman461 <karamandeniz363@gmail.com>
2026-04-12 21:52:24 +0300

    LeagueManagerTest.java has added

--- src/test/java/com/sportsmanager/core/LeagueManagerTest.java
+++ src/test/java/com/sportsmanager/core/LeagueManagerTest.java
@@ -1,4 +1,93 @@
 package com.sportsmanager.core;
 
+import com.sportsmanager.core.LeagueManager;
+import com.sportsmanager.model.common.League;
+import com.sportsmanager.model.common.StandingsEntry;
+import com.sportsmanager.model.sports.football.FootballPlayer;
+import com.sportsmanager.model.sports.football.FootballTeam;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LeagueManagerTest {
+
+    private LeagueManager leagueManager;
+    private FootballTeam team1;
+    private FootballTeam team2;
+    private FootballTeam team3;
+
+    @BeforeEach
+    void setUp() {
+        leagueManager = new LeagueManager();
+        team1 = new FootballTeam("Team 1");
+        team2 = new FootballTeam("Team 2");
+        team3 = new FootballTeam("Team 3");
+
+        for (int i = 0; i < 11; i++) {
+            team1.addPlayer(new FootballPlayer("Player " + i, 75));
+            team2.addPlayer(new FootballPlayer("Player " + i, 72));
+            team3.addPlayer(new FootballPlayer("Player " + i, 70));
+        }
+    }
+
+    @Test
+    void leagueShouldHaveCorrectTeamCount() {
+        League league = leagueManager.createLeague("Test League", Arrays.asList(team1, team2, team3));
+        assertEquals(3, league.getTeams().size());
+    }
+
+    @Test
+    void standingsShouldHaveEntryForEachTeam() {
+        League league = leagueManager.createLeague("Test League", Arrays.asList(team1, team2, team3));
+        List<StandingsEntry> standings = leagueManager.calcStandings(league);
+        assertEquals(3, standings.size());
+    }
+
+    @Test
+    void initialPointsShouldBeZero() {
+        League league = leagueManager.createLeague("Test League", Arrays.asList(team1, team2));
+        List<StandingsEntry> standings = leagueManager.calcStandings(league);
+        for (StandingsEntry entry : standings) {
+            assertEquals(0, entry.getPoints());
+        }
+    }
+
+    @Test
+    void winShouldGiveThreePoints() {
+        StandingsEntry entry = new StandingsEntry(team1);
+        entry.addResult(2, 0);
+        assertEquals(3, entry.getPoints());
+    }
+
+    @Test
+    void drawShouldGiveOnePoint() {
+        StandingsEntry entry = new StandingsEntry(team1);
+        entry.addResult(1, 1);
+        assertEquals(1, entry.getPoints());
+    }
+
+    @Test
+    void lossShouldGiveZeroPoints() {
+        StandingsEntry entry = new StandingsEntry(team1);
+        entry.addResult(0, 2);
+        assertEquals(0, entry.getPoints());
+    }
+
+    @Test
+    void goalDifferenceShouldBeCorrect() {
+        StandingsEntry entry = new StandingsEntry(team1);
+        entry.addResult(3, 1);
+        assertEquals(2, entry.getGoalDifference());
+    }
+
+    @Test
+    void playedCountShouldIncrease() {
+        StandingsEntry entry = new StandingsEntry(team1);
+        entry.addResult(1, 0);
+        entry.addResult(2, 1);
+        assertEquals(2, entry.getPlayed());
+    }
+    //DenizKaraman461
 }
