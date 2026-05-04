22bbfd50cc0c61d0733cc80895e4098d79c787ab
ysfeylmz <yusufafjd@gmail.com>
2026-05-04 10:56:49 +0300

    .

---
+++ SportManager/src/main/java/org/example/model/common/Fixture.java
@@ -0,0 +1,7 @@
+package org.example.model.common;
+
+public class Fixture {
+    Map<Integer, List<Match>> weeklyMatches;
+
+    List<Match> getMatchesByWeek(int week);
+}
---
+++ SportManager/src/main/java/org/example/model/common/League.java
@@ -0,0 +1,8 @@
+package org.example.model.common;
+
+public class League {
+    String name;
+    List<Team> teams;
+    Fixture fixture;
+    List<StandingsEntry> standings;
+}
